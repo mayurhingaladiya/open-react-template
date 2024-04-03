c9a6f63e75d3a1b610cfa32700906f9c362c4b47
mayurhingaladiya <mayxztv@gmail.com>
2024-04-03 12:00:00 +0100

    added markscheme for paper 2

--- components/gcsePredicted.tsx
+++ components/gcsePredicted.tsx
@@ -10,20 +10,19 @@ function gcsePredicted() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-8 md:pb-16">
             <h2 className="h2 mb-4">Predicted Papers</h2>
-            <p className="text-xl text-gray-500">Let's get the tiktok page to 10k followers to help as many people out as possible! Paper 2 coming soon. I won't be uploading a markscheme but will instead go through this paper LIVE and also upload a walkthrough on my youtube channel. So go subscribe and drop a follow!!!!!</p>
+            <p className="text-xl text-gray-500">Let's get the tiktok page to 10k followers and youtube to 1k to help as many people out as possible!</p>
           </div>
           <div className='flex flex-col text-blue-700'>
             <a href="/PredictedPapers/OCR Predicted Paper 1 2024.pdf" target="_blank" className='w-fit mx-auto' rel="noopener noreferrer">Predicted Paper 1 - OCR SPEC</a>
             <a href="/PredictedPapers/OCR Predicted Paper 1 2024Markscheme.pdf" target="_blank" className='w-fit mx-auto' rel="noopener noreferrer">Predicted Paper 1 Answers - OCR SPEC</a>
             <a href="/PredictedPapers/OCR Predicted Paper 2 2024.pdf" target="_blank" className='w-fit mx-auto' rel="noopener noreferrer">Predicted Paper 2 - OCR SPEC</a>
-            <a href="/PredictedPapers/OCR Predicted Paper 2 2024.pdf" target="_blank" className='w-fit mx-auto' rel="noopener noreferrer">Predicted Paper 2 Answers - OCR SPEC</a>
+            <a href="/PredictedPapers/OCR Predicted Paper 2 2024Markscheme.pdf" target="_blank" className='w-fit mx-auto' rel="noopener noreferrer">Predicted Paper 2 Answers - OCR SPEC</a>
 
           </div>
 
           <div className=''> 
           <iframe className='mx-auto mt-10 h-96 w-[350px] md:w-[550px]' src="https://www.youtube.com/embed/9lH-cz4yhkA" title="2024 OCR GCSE Computer Science Predicted Paper 1 Walkthrough" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-          <iframe className='mx-auto mt-10 h-96 w-[350px] md:w-[550px]' src="https://www.youtube.com/embed/_oTKvD1rn5w" title="2024 OCR GCSE Computer Science Predicted Paper 2 Walkthrough" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-
+          <iframe className='mx-auto mt-10 h-96 w-[350px] md:w-[550px]' src="https://www.youtube.com/embed/gN0yZe7qdsU" title="2024 OCR GCSE Computer Science Predicted Paper 2 Walkthrough" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
           </div>
 
 
